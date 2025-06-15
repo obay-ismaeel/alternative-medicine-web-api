dc2c2a97630beec8a48cbaab3dd902ff92f9974d
Eyas Wannous <eyaswannous@gmail.com>
2025-06-15 23:27:39 +0300

    Add File Comparer with new update product API

--- AlternativeMedicine.App/Services/IFileStorageService.cs
+++ AlternativeMedicine.App/Services/IFileStorageService.cs
@@ -6,4 +6,5 @@ public interface IFileStorageService
 {
     public Task<string> StoreAsync(IFormFile file);
     public void Delete(string? filePath);
+    byte[]? GetImageBytes(string filePath);
 }
